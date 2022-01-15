7b2e54c5da6c5d59ecf476ea872155e4cd16f434
Conor Roberts <62777912+ConorRoberts@users.noreply.github.com>
2022-01-14 20:00:15 -0500

    updates

--- A1/FFWIndices.f95
+++ A1/FFWIndices.f95
@@ -1,250 +1,101 @@
-
-
-! C     FORTRAN IV PROGRAM TO CALCULATE CANADIAN FOREST
-! C     FIRE WEATHER INDEX FOR A DEC PDP 11 AT P.F.E.S.
-! C     READS DATA AND PRINTS OUT IN METRIC UNITS.
-!       DIMENSION LMON(12), EL(12), FL(12)
-!       WRITE(*,1004)
-! 1004  FORMAT(2X,'PROGRAM NO.: F-4')
-! 100   FORMAT(I2,F4.1,F1.1)
-! 101   FORMAT(F4.1,I4,I4,F4.1)
-! 102   FORMAT(F6.1,F6.1,F6.1,I2,I2)
-! C     READS LENGTH OF MONTHS, AND DAY LENGTH FACTORS
-!       DO 20 J=1,12
-!       READ(*,100) LMON(J), EL(J), FL(J)
-!       WRITE(*,) LMON(J), EL(J), FL(J)
-! 20    CONTINUE
-! C     READS INITIAL VALUES OF FFMC, DMC, DC, STARTING MONTH AND NUMBER
-! C     OF DAYS OF DATA STARTING MONTH.
-!       READ(*,102) FO,PO,DOT,M,NDAYS
-!       DO 25 J=M,12
-!       NN=LMON(J)
-! 1002  FORMAT(10(/),1X,'  DATE  TEMP  RH  WIND  RAIN  FFMC  DMC  DC
-!      1    ISI   BUI   FWI'/)
-!       IF(J.EQ.M) GO TO 304
-!       IDAYS=1
-!       GO TO 302
-! 304   IDAYS=LMON(J)-NDAYS+1
-! C     READS DAILY WEATHER DATA
-! 302   L=0
-!       DO 25 I=IDAYS,NN
-!       L=L+1
-!       READ(*,101,END=2000) T,IH,IW,R
-!       IF(L.NE.1) GO TO 301
-!       WRITE(*,1002)
-! 301   TX=T
-!       H=IH
-!       W=IW
-!       RAIN=R
-! C     FINE FUEL MOISTURE CODE
-!       IF(R.GT.0.5) GO TO 10
-!       R=0.0
-!       FR=FO
-!       GO TO 150
-! 10    RA=R
-!       IF(RA.LE.1.45) GO TO 6
-!       IF(RA-5.75) 9,9,12
-! 6     F=123.85-(55.6*ALOG(RA+1.016))
-!       GO TO 13
-! 9     F=57.87-(18.2*ALOG(RA-1.016))
-!       GO TO 13
-! 12    F=40.69-(8.25*ALOG(RA-1.905))
-! 13    C=8.73*EXP(-0.1117*FO)
-!       FR=(FO/100.)*F+(1.0-C)
-!       IF(FR.GE.0.) GO TO 150
-!       fr=0.0
-! 150   WMO=101.-FR
-!       ED=0.942*(H**0.679)+(11.*EXP((H-100.)/10.))+0.18*(21.2-T)
-!      1*(1.-1./EXP(0.115*H))
-!       IF(WMO-ED) 26,27,28
-! 26    EW=0.618*(H**0.753)+(10.*EXP((H-100.)/10.))+0.18*(21.1-T)
-!      1*(1.-1./EXP(0.115*H))
-!       IF(WMO.LT.EW) GO TO 29
-! 27    WM=WMO
-!       GO TO 30
-! 28    Z=0.424*(1.-(H/100.)**1.7)+(0.0694*(W**0.5))*(1.-(H/100.)**8)
-!       X=Z*(0.463*(EXP(0.0365*T)))
-!       WM=ED+(WMO-ED)/10.**X
-!       GO TO 30
-! 29    WM=EW-(EW-WMO)/1.9953
-! 30    FFM=101.-WM
-!       IF(FFM.GT.101.) GO TO 32
-!       IF(FFM) 33,34,34
-! 32    FFM=101.
-!       GO TO 34
-! 33    FFM=0.0
-! C     DUFF MOISTURE CODE
-! 34    IF(T+1.1.GE.0.) GO TO 41
-!       T=-1.1
-! 41    RK=1.894*(T+1.1)*(100.-H)*(EL(J)*0.0001)
-! 43    IF(R.GT.1.5) GO TO 45
-!       PR=PO
-!       GO TO 250
-! 45    RA=R
-!       RW=0.92*RA-1.27
-!       WMI=20.0+280./EXP(0.023*PO)
-!       IF(PO.LE.33.) GO TO 50
-!       IF(PO-65.) 52,52,53
-! 50    B=100./(0.5+0.3*PO)
-!       GO TO 55
-! 52    B=14.-1.3*ALOG(PO)
-!       GO TO 55
-! 53    B=6.2*ALOG(PO)-17.2
-! 55    WMR=WMI+(1000.*RW)/(48.77+B*RW)
-!       PR = 43.43*(5.6348-ALOG(WMR-20.))
-! 250   IF(PR.GE.0.) GO TO 61
-!       PR=0.0
-! 61    DMC=PR+RK
-! C     DROUGHT CODE
-!       IF(T+2.8.GE.0.) GO TO 65
-!       T=-2.8
-! 65    PE=(.36*(T+2.8)+FL(J))/2.
-!       IF(R.LE.2.8) GO TO 300
-!       RA=R
-!       RW=0.83*RA-1.27
-!       SMI=800.*EXP(-DOT/400.)
-!       DR=DOT-400.*ALOG(1.+((3.937*RW)/SMI))
-!       IF(DR.GT.0.) GO TO 83
-!       DR=0.0
-! 83    DC=DR+PE
-!       GO TO 350
-! 300   DR=DOT
-!       GO TO 83
-! 350   IF(DC.GE.0.) GO TO 85
-!       DC=0.0
-! C     INITIAL SPREAD INDEX, BUILDUP INDEX, FIRE WEATHER INDEX
-! 85    FM=101.-FFM
-!       SF=19.115*EXP(-0.1386*FM)*(1.+FM**4.65/7950000.)
-!       SI=SF*EXP(0.05039*W)
-! 93    BUI=(0.8*DC*DMC)/(DMC+0.4*DC)
-!       IF(BIU.GE.DMC) GO TO 95
-!       P=(DMC-BUI)/DMC
-!       CC=0.92+(0.0114*DMC)**1.7
-!       BUI=DMC-(CC*P)
-!       IF(BUI.LT.0.) BUI=0.
-! 95    IF(BUI.LT.80.) GO TO 60
-!       BB=0.1*SI*(0.626*BUI**0.809+2.)
-!       GO TO 91
-! 60    BB=0.1*SI*(1000./(25.+108.64/EXP(0.023*BUI)))
-! 91    IF(BB-1.0.LE.0.) GO TO 98
-!       SL=2.72*(0.43*ALOG(BB))**0.647
-!       FWI=EXP(SL)
-!       GO TO 400
-! 98    FWI=BB
-! 400   IDC=DC+0.5
-!       IFFM=FFM+0.5
-!       IDMC=DMC+0.5
-!       ISI=SI+0.5
-!       IBUI=BUI+0.5
-!       IFWI=FMI+0.5
-!       WRITE(*,1001) J,I,TX,IH,IW,RAIN,IFFM,IDMC,IDC,ISI,IBUI,IFWI
-! 1001  FORMAT(1X,2I3,F6.1,I4,I6,F7.1,6I6)
-!       FO=FFM
-!       PO=DMC
-!       DOT=DC
-! 25    CONTINUE
-! 2000  STOP
-!       END
-! 
-
-! Convert code above to f95
-
 module FFWIndices
 implicit none
 
   real :: e = 2.71828182845904523536028747135266249775724709369995
 
 contains
+    integer function get_ffmc(temperature,humidity,wind_speed,previous_moisture)
+      real :: temperature,previous_moisture
+      integer:: humidity,wind_speed
 
-    real function get_temperature()
-      ! Measured in degrees C
-      real :: T=20
-      get_temperature = T
+      ! real :: RO
+
+      ! RO = get_rainfall_function(rainfall)
+
+      ! correction = get_correction(previous_ffmc)
+
+      get_ffmc = 101 - get_moisture(previous_moisture,temperature,humidity,wind_speed)
     end function
 
-    real function get_relative_humidity()
-      ! Measured as %
-      real :: H=20
-      get_relative_humidity = H
+    real function get_moisture(previous_moisture,temperature,humidity,wind_speed)
+      real :: previous_moisture,temperature,rainfall,moisture
+      integer :: humidity,wind_speed
+
+      if (rainfall > 0.5) then
+        moisture = get_wet_moisture(previous_moisture,temperature,humidity)
+      else
+        moisture = get_dry_moisture(previous_moisture,temperature,humidity,wind_speed)
+      end if
+
+      get_moisture = moisture
     end function
 
-    real function get_wind_speed()
-      ! Measured in km/h
-      real :: W=20
-      get_wind_speed = W
+    real function get_log_drying_rate(temperature,humidity,wind_speed)
+      real :: log_drying_rate,temperature
+      integer :: wind_speed,humidity
+
+      log_drying_rate = 0.424*(1.-(humidity/100.)**1.7)+(0.0694*(wind_speed**0.5))*(1.-(humidity/100.)**8)
+      log_drying_rate = log_drying_rate*0.463*(e**(0.0365*temperature))
+
+      get_log_drying_rate = log_drying_rate
     end function
 
-    real function get_rainfall()
-      ! Measured in mm
-      real :: R=20
-      get_rainfall = R
-    end function
-    
-    real function get_rainfall_dmc()
-      ! Measured in mm
-      real :: DMC=20
-      get_rainfall_dmc = DMC
-    end function
-    
-    real function get_rainfall_dc()
-      ! Measured in mm
-      real :: DC=20
-      get_rainfall_dc = DC
+    real function get_wet_moisture(previous_moisture,temperature,humidity)
+      real :: EW,previous_moisture,temperature
+      integer :: humidity
+
+      EW = get_wet_emc(temperature,humidity)
+
+      get_wet_moisture = EW - ((EW - previous_moisture)/1.9953)
     end function
 
-    ! Fine Fuel Moisture Code (FFMC)
+    real function get_dry_moisture(previous_moisture,temperature,humidity,wind_speed)
+      real :: previous_moisture,temperature,k,ED
+      integer :: wind_speed,humidity
 
-    real function get_previous_ffmc(H,W)
-      ! k0
+      ED = get_wet_emc(temperature,humidity)
 
-      real :: H,W
-      get_previous_ffmc = 0.424*(1.-(H/100.)**1.7)+(0.0694*(W**0.5))*(1.-(H/100.)**8)
+      k = get_log_drying_rate(temperature,humidity,wind_speed)
+
+      get_dry_moisture = ED + ((previous_moisture - ED) * (10 ** (-k)));
     end function
 
-    real function get_ffmc(T,H,W)
-      ! k
-      real:: H,W,T
-      get_ffmc = get_previous_ffmc(H,W) * 0.463**(0.0365*T)
+    real function get_dry_emc(temperature,humidity)
+      real :: temperature
+      integer :: humidity
+      get_dry_emc = 0.942*(humidity**0.679)+(11*(e**((humidity-100)/10))+0.18*(21.1-temperature)*(1-e**(-0.115*humidity)))
     end function
 
-    real function get_dry_ffmc()
-      ! Ed
-      real :: ED=20
-      get_dry_ffmc = ED
+    real function get_wet_emc(temperature,humidity)
+      real :: temperature
+      integer :: humidity
+      get_wet_emc = 0.618*(humidity**0.753)+(10*(e**((humidity-100)/10))+0.18*(21.1-temperature)*(1-e**(-0.115*humidity)))
     end function
 
-    ! real function get_wet_ffemc()
-    !   ! Ew
-    !   get_wet_ffemc = 
-    ! end function
+    real function get_rainfall_function(rainfall)
+      real :: new_rainfall,rainfall
 
-    real function get_ffmc_ed(T,H,W)
-      ! m (ED)
-      real :: T,H,W,m_0,ED
-      get_ffmc_ed = ED + (M_0 - ED) * 10**(-get_ffmc(T,H,W))
+      if (rainfall > 0.5 .AND. rainfall <= 1.45) then
+        new_rainfall = 123.85-(55.6*ALOG(rainfall+1.016))
+      else if (rainfall > 1.45 .AND. rainfall <= 5.75) then
+        new_rainfall = 57.87-(18.2*ALOG(rainfall-1.016))
+      else if (rainfall > 5.75) then
+        new_rainfall = 40.69-(8.25*ALOG(rainfall-1.905))
+      end if
+
+      get_rainfall_function = new_rainfall
     end function
 
-    ! real function get_ffmc_ew()
-    !   ! m (EW)
-    !   get_ffmc_ew = 
-    ! end function
+    real function get_correction(previous_ffmc)
+      real :: previous_ffmc
 
-    real function get_k_intermediate()
-      real :: K=20
-      get_k_intermediate = K
+      get_correction = 8.73 * (e ** (-0.1117 * previous_ffmc))
     end function
 
-    real function get_log_drying_rate()
-      ! k
-      ! Measured as m/day
-      real :: K=20
-      get_log_drying_rate = K
-    end function
+    real function get_fr(previous_rainfall)
+      real :: previous_rainfall,previous_ffmc
 
-    real function get_correction_term()
-      ! Measured as m/day
-      real :: C=20
-      get_correction_term = C
+      get_fr = (previous_ffmc / 100 * get_rainfall_function(previous_rainfall)) + 1 - get_correction(previous_ffmc)
     end function
     
 end module FFWIndices
--- A1/ffwi.f95
+++ A1/ffwi.f95
@@ -5,19 +5,23 @@ program ffwi
     character(len=100) :: file_name=""
     integer, dimension(12) :: month_lengths
     real, dimension(12) :: dmc_factors,dc_factors
-    integer :: i=0,j=0
+    integer :: i=0,days_left=0,current_month=0,current_day=0
+    real :: current_dmc,previous_dmc,current_ffmc,previous_ffmc,current_dc,previous_dc
+    real :: temperature,rainfall,previous_moisture
+    integer:: humidity,wind_speed,ffmc,dmc,dc,isi,bui,fwi
 
     ! Get filename from stdin
-    print *,"Please enter the filename of the file containing the FFW indices: "
+    print *,"Please enter the name of the file containing the FFW indices: "
     read *,file_name
 
     ! Declare formats
     write(*,104)
-104 format(2X,'PROGRAM NO.: F-4')
 100 format(I2,F4.1,F4.1)
 101 format(F4.1,I4,I4,F4.1)
 102 format(F4.1,F4.1,F5.1,I2,I2)
-103 format(10(/),1X,'  DATE  TEMP  RH   WIND  RAIN   FFMC   DMC   DC   1   ISI   BUI   FWI'/)
+103 format(10(/),1X,'  DATE  TEMP  RH   WIND  RAIN   FFMC   DMC   DC   ISI   BUI   FWI'/)
+104 format(2X,'PROGRAM NO.: F-4')
+105 format(1X,2I3,F6.1,I4,I6,F7.1,6I6)
 
     ! Open file (corresponding to filename) and month data
     open(unit=1,file=file_name,status='old')
@@ -27,62 +31,50 @@ program ffwi
         read(1,100) month_lengths(i),dmc_factors(i),dc_factors(i)
     end do
 
-    read(1,102) fo,po,dot,m,bdays
-    ! READ(1,102) FO,PO,DOT,M,NDAYS
+    read(1,102) current_ffmc,current_dmc,current_dc,current_month,days_left
 
+    previous_dc = current_dc
+    previous_dmc = current_dmc
+    previous_ffmc = current_ffmc
 
+    ! Print column headers
+    write (*,103)
 
-    ! Print header
-    ! print *,103
+    ! Get initial current day
+    current_day = month_lengths(current_month) - days_left+1
 
-    ! Print data
-    ! do i=days_in_starting_month,month_lengths(starting_month)
+    ! Main loop
+    do
+        read(1,101,END=400) temperature,humidity,wind_speed,rainfall
 
-    ! end do
+        ffmc=get_ffmc(temperature,humidity,wind_speed,previous_moisture)
+        dmc=0
+        dc=0
+        isi=0
+        bui=0
+        fwi=0
+        
+        ! Print data
+        write(*,105) current_month,current_day,temperature,humidity,wind_speed,rainfall,ffmc,dmc,dc,isi,bui,fwi
 
+        previous_ffmc = ffmc
+        previous_dc = dc
+        previous_dmc = dmc
+        
+        ! Check if we are beyond the end of the current month
+        if (current_day==month_lengths(current_month)) then
+            ! Print header
+            write (*,103)
 
-        ! WRITE(*,1001) J,I,TX,IH,IW,RAIN,IFFM,IDMC,IDC,ISI,IBUI,IFWI
-! 1001  FORMAT(1X,2I3,F6.1,I4,I6,F7.1,6I6)
-!    end do
-
-    ! Read initial values
-    ! READS INITIAL VALUES OF FFMC, DMC, DC, STARTING MONTH AND NUMBER OF DAYS OF DATA STARTING MONTH.
-    ! read(1,*) FO,PO,DOT,M,NDAYS
-
-    ! do j=m,12
-    !     if(j==m) then
-    !         IDAYS=LMON(J)-NDAYS+1
-    !     else
-    !         IDAYS=1
-    !     end if
-    ! end do
-    ! NN=LMON(J)
-    ! L=0
-    ! do i=IDAYS,NN
-    !     L=L+1
-    ! end do
-    ! READ(*,*) T,IH,IW,R
-    ! IF(L/=1) GO TO 301
-    ! WRITE(*,1002)
-    ! TX=T
-    ! H=IH
-    ! W=IW
-    ! RAIN=R
-
-    ! integer :: current_day=1
-    ! integer :: current_f = 0
-
-    ! Previous day's F becomes F0
-    ! If R0>0.5, calculate F(R0) 
-    ! if (current_day>1) then
-        ! days(current_day-1) = current_f
-
-    ! Calculate FFMC
-
+            ! Increment month and reset day
+            current_month = current_month + 1
+            current_day = 1
+        else
+            ! Increment current day
+            current_day = current_day + 1
+        end if
+    end do
     
-   
-    ! Print out headers
-    ! print *,"  DATE  TEMP  RH   WIND  RAIN   FFMC   DMC   DC   1   ISI   BUI   FWI"
-    
-    close(1)
+400 close(1)
+
 end program ffwi
